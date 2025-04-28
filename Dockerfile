24f1f9c6c6db8db2f1a2fc45223142b3f71e1bd4
1kamson2 <qwerty21028@gmail.com>
2025-04-28 16:25:34 +0200

    fix

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-from httpd:latest
-
-COPY html/ /usr/local/apache2/htdocs/
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "[INFO] Starting up the server."
-docker run -d -p 8080:80 \
-  -v "$(pwd)/html":/usr/local/apache2/htdocs/ \
-  --name apache-dev httpd:2.4
